e41f8a8174e86e4c68b04d98d30d7969031fab4e
Nurzhan Bizhan <bknurik@gmail.com>
2022-11-30 18:51:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 import { tweetsData } from './data.js';
-
-const tweetBtn = document.querySelector('#tweet-btn');
-const tweetInput = document.querySelector('#tweet-input');
-
-tweetBtn.addEventListener('click', () => {
-  if (tweetInput.value !== '') {
-    tweetInput.value = '';
-  }
-});
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 document.addEventListener('click', (e) => {
-  const like = e.target.dataset.like;
-  if (like) {
-    handleLikeClick(like);
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
+  }
+  if (e.target.id === 'tweet-btn') {
+    handleTweetBtnClick();
   }
 });
 
@@ -23,19 +21,63 @@ function handleLikeClick(tweetId) {
 
   if (!targetTweetObj.isLiked) {
     targetTweetObj.likes++;
-    targetTweetObj.isLiked = true;
   } else {
-    targetTweetObj.isLiked = false;
     targetTweetObj.likes--;
   }
 
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+
   render();
+}
+
+function handleRetweetClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(
+    (tweet) => tweet.uuid === tweetId
+  )[0];
+
+  if (!targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets++;
+  } else {
+    targetTweetObj.retweets--;
+  }
+
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+
+  render();
+}
+
+function handleReplyClick(tweetId) {
+  document.getElementById(`replies-${tweetId}`).classList.toggle('hidden');
+}
+
+function handleTweetBtnClick() {
+  const tweetInput = document.querySelector('#tweet-input');
+
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@Scrimba`,
+      profilePic: `images/scrimbalogo.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    });
+
+    tweetInput.value = '';
+
+    render();
+  }
 }
 
 function getFeedHtml() {
   let feedHtml = ``;
 
   tweetsData.forEach((tweet) => {
+    let likedClass = '';
+    let retweetClass = '';
     const {
       handle,
       profilePic,
@@ -47,6 +89,32 @@ function getFeedHtml() {
       isRetweeted,
       uuid,
     } = tweet;
+
+    if (isRetweeted) {
+      retweetClass = 'retweeted';
+    }
+
+    if (isLiked) {
+      likedClass = 'liked';
+    }
+
+    let repliesHtml = '';
+
+    if (replies.length > 0) {
+      replies.forEach((replie) => {
+        repliesHtml += `
+        <div class="tweet-reply">
+          <div class="tweet-inner">
+              <img src="${replie.profilePic}" class="profile-pic">
+                  <div>
+                      <p class="handle">${replie.handle}</p>
+                      <p class="tweet-text">${replie.tweetText}</p>
+                  </div>
+          </div>
+        </div>
+     `;
+      });
+    }
 
     feedHtml += `
         <div class="tweet">
@@ -61,16 +129,19 @@ function getFeedHtml() {
                           ${replies.length}
                       </span>
                       <span class="tweet-detail">
-                      <i class="fa-solid fa-heart" data-like="${uuid}"></i>
+                      <i class="fa-solid fa-heart ${likedClass}" data-like="${uuid}"></i>
                           ${likes}
                       </span>
                       <span class="tweet-detail">
-                      <i class="fa-solid fa-retweet" data-retweet="${uuid}"></i>
+                      <i class="fa-solid fa-retweet ${retweetClass}" data-retweet="${uuid}"></i>
                           ${retweets}
                       </span>
                   </div>   
               </div>            
           </div>
+          <div class="hidden" id="replies-${uuid}">
+            ${repliesHtml}
+          </div>  
       </div>
     `;
   });
